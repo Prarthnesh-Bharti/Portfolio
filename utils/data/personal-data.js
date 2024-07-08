143f9ba81665f49c1dddd68a09bdc223f6d0e849
Prarthnesh Bharti <prarthneshbharti@gmail.com>
2024-07-09 03:08:06 +0530

    first commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "PRARTHNESH BHARTI",
+  profile: '/profile.jpg',
+  designation: "Web Developer",
+  description: "My name is Prarthnesh Bharti. I am an engineering student at NIT Patna. I am an enthusiastic programmer with a quick learning attitude and a passion for problem-solving. I love to learn and explore new technologies, especially in web application development. My core skill is based on JavaScript, and I enjoy using it for most of my projects. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'prarthneshbharti@gmail.com',
+  phone: '+918114548226',
+  address: 'Hajipur , Bihar, India',
+  github: 'https://github.com/Prarthnesh-Bharti',
+  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  linkedIn: 'https://www.linkedin.com/in/prarthnesh-bharti-084795287/',
+  twitter: 'https://x.com/prarthnesh',
+  stackOverflow: 'https://www.linkedin.com/in/prarthnesh-bharti-084795287/',
+  leetcode: "https://leetcode.com/u/prarthnesh/",
+  devUsername: "Prarthnesh-Bharti",
+  resume: "https://www.linkedin.com/in/prarthnesh-bharti-084795287/"
+}
