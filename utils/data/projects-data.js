739d7986997d4b93fc7f838d966689d2a0502060
Prarthnesh Bharti <128624879+Prarthnesh-Bharti@users.noreply.github.com>
2024-10-08 05:18:20 +0530

    Update projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -7,7 +7,7 @@ export const projectsData = [
     {
         id: 1,
         name: 'SORTING VISUALISER',
-        description: "It is a robust and scalable RESTful API and it provides seamless integration and interaction for users and applications.",
+        description: "I have made this project using my understanding data structure and algorithm  which is based on various sorting algorithm.",
         tools: ['HTML', 'CSS', "Javascript"],
         role: 'Backend Developer',
         code: '',
@@ -28,8 +28,8 @@ export const projectsData = [
     {
         id: 3,
         name: 'PERSONAL PORTFOLIO',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['Nextjs', 'SCSS', 'JavaScript'],
+        description: 'I have developed a fully responsive portfolio using my frontend skill. '
+        tools: ['HTML', 'CSS', "Javascript"],
         code: '',
         role: 'Full Stack Developer',
         demo: '',
@@ -39,7 +39,7 @@ export const projectsData = [
     {
         id: 4,
         name: 'CRIME MANAGEMENT SYSTEM',
-        description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
+        description: "My team and I developed Crime management system where I integrated google map and a emergency button. 
         tools: ['HTML', 'CSS', 'Javacsript'],
         code: '',
         demo: '',
