c394b4be05a3aa874470346e9d87cdb673054738
Prarthnesh Bharti <128624879+Prarthnesh-Bharti@users.noreply.github.com>
2024-10-08 04:44:51 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -14,7 +14,7 @@ export const experiences = [
   },
   {
     id: 3,
-    title: "Internship ",
+    title: "Industrial Trainee ",
     company: "North Bihar Power Distribution Ltd. Hajipur",
     duration: "July 2023 -July 2023"
   }
